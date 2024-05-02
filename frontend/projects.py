f0177bbd83abb652d4ba9d373f744cc2ca4dbdb7
Stephany Vargas <yostephy@hotmail.com>
2024-05-02 14:21:48 +0900

    deleted hugging face

--- frontend/projects.py
+++ frontend/projects.py
@@ -1,6 +1,6 @@
 # projects.py
 import streamlit as st
-import hugging_face
+#import hugging_face
 import gaussian_processes
 import practical_statistics
 
@@ -8,15 +8,11 @@ def load_page():
     st.write("## Projects")
 
     # Nested Projects Menu
-    project_selection = st.sidebar.radio("Choose a Project:", ["Practical Statistics",
-                                                               "Hugging Face", 
+    project_selection = st.sidebar.radio("Choose a Project:", ["Practical Statistics", 
                                                                "Gaussian Processes"])
 
     if project_selection == "Practical Statistics":
         practical_statistics.load_page()
-        
-    elif project_selection == "Hugging Face":
-        hugging_face.load_page()
 
     elif project_selection == "Gaussian Processes":
         gaussian_processes.load_page()
