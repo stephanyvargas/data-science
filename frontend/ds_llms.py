cb6de9272f2e1da0a5c6eb584241d0fb72109869
Stephany Vargas <yostephy@hotmail.com>
2024-05-07 09:13:22 +0900

    add eval prometheus 2

--- frontend/ds_llms.py
+++ frontend/ds_llms.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pages import hugging_face
+from pages import hugging_face, paper_prometheus
 
 def load_page():
     st.write("## LLMs for Data Science")
@@ -8,7 +8,10 @@ def load_page():
     projects_expander = st.expander("Choose a Topic")
 
     with projects_expander:
-        project_selection = st.radio("", ["Hugging Face"])
+        project_selection = st.radio("", ["Hugging Face", "Evaluate LLMs: Prometheus2"])
 
     if project_selection == "Hugging Face":
         hugging_face.load_page()
+
+    elif project_selection == "Evaluate LLMs: Prometheus2":
+        paper_prometheus.load_page()
